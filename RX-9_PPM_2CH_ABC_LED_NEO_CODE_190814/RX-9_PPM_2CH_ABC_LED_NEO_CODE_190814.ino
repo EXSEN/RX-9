76a41a15f8b2963b87bf68a99b500f6c38cff32d
Joseph Kim <ykkim@exsen.co.kr>
2019-08-16 21:58:41 +0900

    ALGORITHM FOR RX_9 IS CHANGED

--- RX-9_PPM_2CH_ABC_LED_NEO_CODE_190814/RX-9_PPM_2CH_ABC_LED_NEO_CODE_190814.ino
+++ RX-9_PPM_2CH_ABC_LED_NEO_CODE_190814/RX-9_PPM_2CH_ABC_LED_NEO_CODE_190814.ino
@@ -1,19 +1,36 @@
 /*
  * coded by EXSEN 
- * date: 2019.07.12 
- * EMF pin connected to A0 of Arduino
+ * date: 2019.08.14  
+ * EMF pin connected to A1 of Arduino
+ * THER pin connected to A0 of Arduino
  * 3.3V external power supply connected to V of RX-9 simple
- * GND connected to arduino
- * GND connected external power  supply
- * file name = RX-9_PPM_1CH_ABC_CODE
+ * RX-9 GND connected to arduino
+ * Arduino GND connected external power  supply
+ * LED_R connected to D5 of Arduino
+ * LED_G connected to D6 of Arduino
+ * LED_B connected to D8 of Arduino
+ * file name = RX-9_NEO_SIMPLE_2CH_LED_CODE
+ * source code = GITHUB.com/EXSEN/RX-9
  * tested by arduino nano V3.0 328p
- * Add ABC algorithm
  */
 
 #include <EEPROM.h>
 
 //EARTH
 int CO2_EARTH = 414; //ABC
+int CO2_BASE_Compensation = 35; //EXSEN nearby road in downtown
+float under_cut = 0.99;
+/* if you live in city, you add more value. 
+ * below is normal compensation value at your residence
+ * nearby heavy traffic road = 40~80;
+ * downtown = 20~40;
+ * Suburb = 10~20;
+ * Country apart from road = 0~10;
+ * Country nearby road = 10~20;
+ * 
+ */
+
+
 
 //Timing Setting
 int warm_up_time = 180;
@@ -24,8 +41,8 @@ int set_time = 1;
 
 
 //PIN
-int EMF_PIN = 0;
-int THER_PIN = 1;
+int EMF_PIN = 1;
+int THER_PIN = 0;
 
 //moving averaging of Sensor value
 int averaging_count_EMF = 10;
@@ -81,20 +98,20 @@ bool Reset_mode = 0;
 int INIT_OFF = 100;
 
 //STEP of co2
-float V1 = 0.9060;
-float V2 = 0.8345;
-float V3 = 0.7557;
+int V1 = 700;
+int V2 = 1000;
+int V3 = 2000;
 int STEP_status = 0;
 float STEP_CO2 = 0.0;
 
 //Calibration data
-float cal_A = 1.734; //<--- QR CODE DATA
-float cal_B = 0.2770; //<--- QR CODE DATA
+float cal_A = 465; //<--- QR CODE DATA
+float cal_B = 65.3; //<--- QR CODE DATA
 float CO2_ppm = 0.0;
-float DEDT = 1.67; //<--- QR CODE DATA
+float DEDT = 1.00; //<--- QR CODE DATA
 
 //Auto Calibration coeff
-int32_t MEIN = 60; //ABC
+int32_t MEIN = 1440; //ABC, 1440 = 1 day, 2880 = 2 day
 int32_t METI = 60; //ABC
 float EMF_max = 0; //ABC
 float THER_max = 0; //2CH
@@ -110,10 +127,26 @@ int32_t CANU = 0; //ABC
 float EMF_offset = 10.24;
 float THER_offset = 10.24;
 
+//LED
+int LED_R = 6;
+int LED_G = 5;
+int LED_B = 8;
+
 void setup(){
   Serial.begin(9600);
   pinMode(sw, INPUT_PULLUP);
+  pinMode(LED_R,OUTPUT); //LED
+  pinMode(LED_G,OUTPUT); //LED
+  pinMode(LED_B,OUTPUT); //LED  
+  digitalWrite(LED_R,HIGH);
+  digitalWrite(LED_G,HIGH);
+  digitalWrite(LED_B,HIGH);
 
+  digitalWrite(LED_R,LOW);
+  digitalWrite(LED_G,LOW);
+  digitalWrite(LED_B,LOW);
+  delay(1000);
+  
   //parameter init
   EMF_ini = EEPROM.read(EMF_ini_ADD_01)*256 + EEPROM.read(EMF_ini_ADD_02) + (float)EEPROM.read(EMF_ini_ADD_03)/100;
   if(EMF_ini<0){EMF_ini = 0;} //ABC
@@ -151,9 +184,13 @@ void loop(){
   current_time = millis()/1000;
   if(current_time - prev_time >= set_time){ //every 1 second, if you change set_time to 60, every 1 minute
     EMF = analogRead(EMF_PIN);
-    delay(5);
+    EMF = EMF/1024; //10 bit resolution
+    EMF = EMF *5; //max voltage = 5V
+    EMF = EMF / 6; // opamp value = 6
+    EMF = EMF * 1000; //mV to V
+    delay(1);
     THER = analogRead(THER_PIN);
-    delay(5);
+    delay(1);
     THER = 1/(C1+C2*log((Resist_0*THER)/(1024-THER))+C3*pow(log((Resist_0*THER)/(1024-THER)),3))-273.15;  
     
     
@@ -205,27 +242,27 @@ void loop(){
 
     //Step calculation START
     if(Sensor_status == 1){
-      STEP_CO2 = EMF_data/EMF_ini;
+      //STEP_CO2 = EMF_data/EMF_ini;
       
-      PPM_CAL(STEP_CO2);
+      PPM_CAL(EMF_data,THER_data);
       //Define STEP START
-      if(STEP_CO2 >= V1 && STEP_CO2 <= 1){
+      if(CO2_ppm <= V1 && CO2_ppm >= 400){
         STEP_status = 0; 
         under_cut_count = 0; //ABC
       }
-      else if(STEP_CO2 < V1 && STEP_CO2 >= V2){
+      else if(CO2_ppm > V1 && CO2_ppm <= V2){
         STEP_status = 1;
         under_cut_count = 0; //ABC
       }
-      else if(STEP_CO2 < V2 && STEP_CO2 >= V3){
+      else if(CO2_ppm > V2 && CO2_ppm <= V3){
         STEP_status = 2;
         under_cut_count = 0; //ABC
       }
-      else if(STEP_CO2 < V3){
+      else if(CO2_ppm >= V3){
         STEP_status = 3;
         under_cut_count = 0; //ABC
       }
-      else if(STEP_CO2 > 1.01){
+      else if(CO2_ppm <= (CO2_EARTH + CO2_BASE_Compensation) * under_cut){
         under_cut_count++; //ABC
         if(under_cut_count>5){ //ABC
           EMF_init(EMF_data, THER_data); //ABC
@@ -238,12 +275,12 @@ void loop(){
       //Define STEP END
 
       //ABC START
-      if(current_time - prev_time_METI >= METI){
+      if(current_time - prev_time_METI >= METI && Sensor_status == 1){
         if(ELTI<MEIN){
           ELTI++;
         }
         else if(ELTI>=MEIN){
-          Auto_CAL(EMF_max, EMF_data, THER_max, THER_data); //2CH, ABC
+          Auto_CAL(); //2CH, ABC
         }
         if(EMF_max >= EMF_data){
           //do nothing
@@ -268,13 +305,23 @@ void loop(){
     Serial.print(CO2_ppm,0);
     Serial.print(" EMF = ");
     Serial.print(EMF_data,0);
-    Serial.print(" EMF_ini = ");
-    Serial.print(EMF_ini,0);
+    Serial.print(" Cal_A = ");
+    Serial.print(cal_A,0);
+    Serial.print(" Cal_B = ");
+    Serial.print(cal_B,0);
     
     Serial.print(" THER = ");
     Serial.print(THER_data,0);
     Serial.print(" THER_ini = ");
     Serial.print(THER_ini,0);
+
+    Serial.print(" ELTI = ");
+    Serial.print(ELTI);
+    
+    Serial.print(" EMF_max = ");
+    Serial.print(EMF_max,0);
+    Serial.print(" THER_max = ");
+    Serial.print(THER_max,0);
     
     Serial.print(" CO2 STEP = ");
     Serial.print(STEP_status);
@@ -282,15 +329,34 @@ void loop(){
     switch(STEP_status){
       case 0:
               Serial.print("Fresh");
+              if(Sensor_status != 1){
+                digitalWrite(LED_G,LOW);
+                digitalWrite(LED_R,LOW);
+                digitalWrite(LED_B,LOW);
+              }
+              else{
+              digitalWrite(LED_G,HIGH);
+              digitalWrite(LED_R,HIGH);
+              digitalWrite(LED_B,LOW);
+              }
               break;
       case 1:
               Serial.print("Good");
+              digitalWrite(LED_G,LOW);
+              digitalWrite(LED_R,HIGH);
+              digitalWrite(LED_B,HIGH);
               break;
       case 2:
               Serial.print("Bad");
+              digitalWrite(LED_G,LOW);
+              digitalWrite(LED_R,LOW);
+              digitalWrite(LED_B,HIGH);
               break;
       case 3:
               Serial.print("Very Bad");
+              digitalWrite(LED_G,HIGH);
+              digitalWrite(LED_R,LOW);
+              digitalWrite(LED_B,HIGH);
               break;
     } 
     Serial.print(", Sensor status =");
@@ -314,18 +380,20 @@ void loop(){
 }
 
 void EMF_init(float EMF_data, float THER_data){
-  EMF_ini = EMF_data;
+  //EMF_ini = EMF_data;
   THER_ini = THER_data; //2CH
-  cal_A = (EMF_data/EMF_ini)+cal_B*log10(CO2_EARTH); //ABC
   EMF_max = EMF_data; //ABC
   THER_max = THER_data; //2CH
   ELTI = 0;  //ABC
   CANU++;  //ABC
+  cal_A = EMF_data +log10(CO2_EARTH + CO2_BASE_Compensation)*cal_B;
   
-  EEPROM.write(EMF_ini_ADD_01,(int)EMF_ini/256);
+  /*
+   * EEPROM.write(EMF_ini_ADD_01,(int)EMF_ini/256);
   EEPROM.write(EMF_ini_ADD_02,(int)EMF_ini%256);
   EEPROM.write(EMF_ini_ADD_03,(int(EMF_ini*100)%100));
-
+  */
+  
   EEPROM.write(cal_A_ADD_01,(int)cal_A/256);
   EEPROM.write(cal_A_ADD_02,(int)cal_A%256);
   EEPROM.write(cal_A_ADD_03,(int(cal_A*100)%100));
@@ -344,16 +412,16 @@ void EMF_init(float EMF_data, float THER_data){
   EEPROM.write(THER_ini_ADD_02,(int(THER_ini*100)%100)); //2CH
 }    
 
-void PPM_CAL(float STEP_CO2){
-  CO2_ppm = pow(10,((cal_A-(EMF_data+DEDT*(THER_ini-THER_data))/(EMF_ini+DEDT*(THER_ini-THER_data)))/cal_B));
+void PPM_CAL(float EMF_data, float THER_data){
+  CO2_ppm = pow(10,((cal_A-(EMF_data+DEDT*(THER_ini-THER_data)))/cal_B));
 }
 
-void Auto_CAL(float EMF_data, float EMF_max, float THER_data, float THER_max){
-  EMF_max = EMF_max; //ABC
-  THER_max = THER_max; //2CH
-  EMF_ini = EMF_data; //ABC
+void Auto_CAL(){
+  cal_A = EMF_max +log10(CO2_EARTH + CO2_BASE_Compensation)*cal_B;
   THER_ini = THER_data; //2CH
-  
+  EMF_max = EMF_data; //ABC
+  THER_max = THER_data; //2CH
+  THER_ini = THER_data; //2CH
   
   ELTI = 0; //ABC
   CANU++;  //ABC
@@ -361,6 +429,10 @@ void Auto_CAL(float EMF_data, float EMF_max, float THER_data, float THER_max){
   EEPROM.write(CANU_ADD_01,CANU/256); //ABC
   EEPROM.write(CANU_ADD_02,CANU%256); //ABC
   
+  EEPROM.write(cal_A_ADD_01,(int)cal_A/256);
+  EEPROM.write(cal_A_ADD_02,(int)cal_A%256);
+  EEPROM.write(cal_A_ADD_03,(int(cal_A*100)%100));
+
   EEPROM.write(EMF_max_ADD_01,(int)EMF_max/256); //ABC
   EEPROM.write(EMF_max_ADD_02,(int)EMF_max%256); //ABC
   EEPROM.write(EMF_max_ADD_03,(int(EMF_max*100)%100)); //ABC
@@ -368,6 +440,8 @@ void Auto_CAL(float EMF_data, float EMF_max, float THER_data, float THER_max){
   EEPROM.write(THER_max_ADD_01,(int)THER_max%256); //2CH
   EEPROM.write(THER_max_ADD_02,(int(THER_max*100)%100)); //2CH
 
+  EEPROM.write(THER_ini_ADD_01,(int)THER_ini%256); //2CH
+  EEPROM.write(THER_ini_ADD_02,(int(THER_ini*100)%100)); //2CH
   
 }
 
--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,2 @@
 INITIATED 2019.07.12
+Algorithm for RX-9 is changed for reduce sensor variation 2019.08.16
